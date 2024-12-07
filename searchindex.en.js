c56f60703c1635ad0c60ed12efaaee3a346a0fe2
kariexx <kariex@protonmail.com>
2024-12-07 21:06:42 +0000

    Deploying to main from @ kkwgo/reno-transport-hugo@4129e6325619f671b0c0c9737c47c7c8de6db21b 🚀

---
+++ searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Transport Fun \u003e Reno Transport Fun",
+    "content": "image with width.\nImage with height\nImage at left\nImage inline",
+    "description": "image with width.\nImage with height\nImage at left\nImage inline",
+    "tags": [],
+    "title": "test page #1",
+    "uri": "/proj1/first/index.html"
+  },
+  {
+    "breadcrumb": "Transport Fun",
+    "content": "Reno Transport Fun.",
+    "description": "Reno Transport Fun.",
+    "tags": [],
+    "title": "Reno Transport Fun",
+    "uri": "/proj1/index.html"
+  },
+  {
+    "breadcrumb": "Transport Fun",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Transport Fun",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Fun stuff about transportations.",
+    "description": "Fun stuff about transportations.",
+    "tags": [],
+    "title": "Transport Fun",
+    "uri": "/index.html"
+  }
+]
